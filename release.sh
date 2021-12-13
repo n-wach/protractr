2ca6dcf45ba111097bef1d3b36f0e4a160ad93f3
n-wach <nmwachholz@gmail.com>
2021-12-12 16:31:20 -0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git checkout gh-pages
-
 rm -rf dist
 rm -rf docs
 
@@ -9,14 +7,25 @@ npm run recompile-typescript
 npm run browserify
 npm run gen-docs
 
-git add dist
-git add docs
-git add index.html
-git add .nojekyll
-git add image
-git add examples
+rm -rf temp
+mkdir temp
+cp -r dist temp/
+cp -r docs temp/
+cp -r examples temp/
+cp -r image temp/
+cp -r styles temp/
+cp index.html temp/
 
+git checkout gh-pages
+
+rm -rf dist docs examples image styles index.html
+mv temp/* .
+
+git add .
 git commit -m "Release for GitHub Pages"
 git push
 
 git checkout main
+rm -rf temp
+rm -rf dist
+rm -rf docs
